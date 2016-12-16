efc23ac99b97260641d09460b72b1cb69d4647f4
Gcaufy <gcaufy@gmail.com>
2016-12-16 12:35:57 +0800

    upgrade config

--- wepy.config.js
+++ wepy.config.js
@@ -2,43 +2,40 @@
 let prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  "wpyExt": ".wpy",
-  "sass": {
-    "outputStyle": "compressed"
-  },
-  "less": {
-    "compress": true
-  },
-  "babel": {
-    "presets": [
-      "es2015",
-      "stage-1"
-    ],
-    "plugins": [
-      "transform-export-extensions",
-      "syntax-export-extensions",
-      "transform-runtime"
-    ]
-  }
+    "wpyExt": ".wpy",
+    "compilers": {
+        "sass": {
+          "outputStyle": "compressed"
+        },
+        babel: {
+            "presets": [
+                "es2015",
+                "stage-1"
+            ],
+            "plugins": [
+                "transform-export-extensions",
+                "syntax-export-extensions",
+                "transform-runtime"
+            ]
+        }
+    }
 };
 
 
 if (prod) {
-  // 压缩sass
-  module.exports['sass'] = {"outputStyle": "compressed"};
+    // 压缩sass
+    module.exports['sass'] = {"outputStyle": "compressed"};
   
-  // 压缩less
-  module.exports['less'] = {"compress": true};
 
-  // 压缩js
-  module.exports.plugins = {
-      'UglifyJsPlugin': {
-          filter: /\.js$/,
-          config: {
-              compress: {
-                  warnings: false
-              }
-          }
-      }
-  };
+    // 压缩js
+    module.exports.plugins = {
+        'UglifyJsPlugin': {
+            filter: /\.js$/,
+            config: {
+                compress: {
+                    warnings: false
+                }
+            }
+        }
+    };
 }
